afe5d198e893d0599c3979f5881caf439c41addc
J-Mara <jmaragakis20@stuy.edu>
2021-01-13 16:59:13 -0500

    first two ironed out

--- Radix.java
+++ Radix.java
@@ -1,6 +1,6 @@
 public class Radix{
   public static int nth(int n, int col){
-    int last = (n % (int) Math.pow(10, (col+1)));
+    int last = (Math.abs(n) % (int) Math.pow(10, (col+1)));
     return last/((int) Math.pow(10, col));
   }
   public static int length(int n){
