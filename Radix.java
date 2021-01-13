efe8eb0d3d90125b6b37d313ec3850ea4bc5ce35
J-Mara <jmaragakis20@stuy.edu>
2021-01-13 18:22:51 -0500

    all but last not iron

--- Radix.java
+++ Radix.java
@@ -14,4 +14,33 @@ public class Radix{
       original.extend(buckets[i]);
     }
   }
+  private static int largest(SortableLinkedList data){
+    int result = 0;
+    for(int i = 0; i < data.size(); i++){
+      if(length(data.get(i)) > result){
+        result = length(data.get(i));
+      }
+    }
+    return result;
+  }
+  public static void radixSortSimple(SortableLinkedList data){
+    SortableLinkedList[] buckets = new SortableLinkedList[10];
+    buckets[0] = new SortableLinkedList();
+    buckets[1] = new SortableLinkedList();
+    buckets[2] = new SortableLinkedList();
+    buckets[3] = new SortableLinkedList();
+    buckets[4] = new SortableLinkedList();
+    buckets[5] = new SortableLinkedList();
+    buckets[6] = new SortableLinkedList();
+    buckets[7] = new SortableLinkedList();
+    buckets[8] = new SortableLinkedList();
+    buckets[9] = new SortableLinkedList();
+    for(int i = 0; i < largest(data); i++){
+      while (data.size() > 0){
+        buckets[nth(data.get(0), i)].add(data.get(0));
+        data.remove(0);
+      }
+      merge(data, buckets);
+    }
+  }
 }
