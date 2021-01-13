ca9afb5e5ef3f768325bf84692e951c4e0c3f407
J-Mara <jmaragakis20@stuy.edu>
2021-01-13 16:57:50 -0500

    first two

--- Radix.java
+++ Radix.java
@@ -3,4 +3,10 @@ public class Radix{
     int last = (n % (int) Math.pow(10, (col+1)));
     return last/((int) Math.pow(10, col));
   }
+  public static int length(int n){
+    if(n == 0){
+      return 1;
+    }
+    return (int)Math.log10(Math.abs(n)) + 1;
+  }
 }
